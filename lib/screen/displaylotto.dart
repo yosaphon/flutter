44c117b3b3b0d9f0b7401d0027ab0aa19b0cbda6
Chaiwat Kanyasai <chaiwat.kulyasai7@gmail.com>
2021-06-29 16:06:40 +0700

    เพิ่มปุ่มวันที่

--- lib/screen/displaylotto.dart
+++ lib/screen/displaylotto.dart
@@ -47,57 +47,101 @@ class _DisplayScreenState extends State<DisplayScreen> {
                   return ListView(
                     children: <Widget>[
                       Container(
-                        padding: const EdgeInsets.only(top: 100.0),
-                        child: Text(
-                          snapshot.data['date'],
-                          style: TextStyle(fontSize: 25),
-                        ),
-                      ),
-                      PrizeBox(//รางวัลที่ 1
+                          padding: const EdgeInsets.only(top: 100.0),
+                          child: TextButton(
+                            style: ButtonStyle(
+                              foregroundColor:
+                                  MaterialStateProperty.all<Color>(Colors.blue),
+                            ),
+                            onPressed: () {
+                              showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return Dialog(
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(40)),
+                                    elevation: 16,
+                                    child: Container(
+                                      child: ListView(
+                                        shrinkWrap: true,
+                                        children: <Widget>[
+                                          SizedBox(height: 20),
+                                          Center(child: Text('เลือกวันที่')),
+                                          SizedBox(height: 20),
+                                        ],
+                                      ),
+                                    ),
+                                  );
+                                },
+                              );
+                            },
+                            child: Text(
+                              snapshot.data['date'],
+                              style: TextStyle(fontSize: 25),
+                            ),
+                          )),
+                      PrizeBox(
+                          //รางวัลที่ 1
                           snapshot.data['prizes'][0]['name'],
                           snapshot.data['prizes'][0]['reward'],
-                          snapshot.data['prizes'][0]['number'],1),
-                          
-                      Row(//รางวัลเลขหน้า, เลขท้าย
+                          snapshot.data['prizes'][0]['number'],
+                          1),
+                      Row(
+                        //รางวัลเลขหน้า, เลขท้าย
                         children: <Widget>[
                           Expanded(
                             child: PrizeBox(
                                 snapshot.data['runningNumbers'][0]['name'],
                                 snapshot.data['runningNumbers'][0]['reward'],
-                                snapshot.data['runningNumbers'][0]['number'],2),
+                                snapshot.data['runningNumbers'][0]['number'],
+                                2),
                           ),
                           Expanded(
                             child: PrizeBox(
                                 snapshot.data['runningNumbers'][1]['name'],
                                 snapshot.data['runningNumbers'][1]['reward'],
-                                snapshot.data['runningNumbers'][1]['number'],2),
+                                snapshot.data['runningNumbers'][1]['number'],
+                                2),
                           ),
                         ],
                       ),
-                      PrizeBox(//รางวัลเลขท้าย 2 ตัว
+                      PrizeBox(
+                          //รางวัลเลขท้าย 2 ตัว
                           snapshot.data['runningNumbers'][2]['name'],
                           snapshot.data['runningNumbers'][2]['reward'],
-                          snapshot.data['runningNumbers'][2]['number'],1),
-                      PrizeBox(//รางวัลใกล้เคียง รางวัลที่ 1
+                          snapshot.data['runningNumbers'][2]['number'],
+                          1),
+                      PrizeBox(
+                          //รางวัลใกล้เคียง รางวัลที่ 1
                           snapshot.data['prizes'][1]['name'],
                           snapshot.data['prizes'][1]['reward'],
-                          snapshot.data['prizes'][1]['number'],2),
-                          PrizeBox(//รางวัลที่ 2
+                          snapshot.data['prizes'][1]['number'],
+                          2),
+                      PrizeBox(
+                          //รางวัลที่ 2
                           snapshot.data['prizes'][2]['name'],
                           snapshot.data['prizes'][2]['reward'],
-                          snapshot.data['prizes'][2]['number'],4),
-                          PrizeBox(//รางวัลที่ 3
+                          snapshot.data['prizes'][2]['number'],
+                          4),
+                      PrizeBox(
+                          //รางวัลที่ 3
                           snapshot.data['prizes'][3]['name'],
                           snapshot.data['prizes'][3]['reward'],
-                          snapshot.data['prizes'][3]['number'],5),
-                          PrizeBox(//รางวัลที่ 4
+                          snapshot.data['prizes'][3]['number'],
+                          5),
+                      PrizeBox(
+                          //รางวัลที่ 4
                           snapshot.data['prizes'][4]['name'],
                           snapshot.data['prizes'][4]['reward'],
-                          snapshot.data['prizes'][4]['number'],5),
-                          PrizeBox(//รางวัลที่ 5
+                          snapshot.data['prizes'][4]['number'],
+                          5),
+                      PrizeBox(
+                          //รางวัลที่ 5
                           snapshot.data['prizes'][5]['name'],
                           snapshot.data['prizes'][5]['reward'],
-                          snapshot.data['prizes'][5]['number'],5),
+                          snapshot.data['prizes'][5]['number'],
+                          5),
                     ],
                   );
                 }
