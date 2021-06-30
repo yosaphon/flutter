e672cb91b5868f96508522446e7639845e60058e
yosaphon <68890091+yosaphon@users.noreply.github.com>
2021-06-30 16:02:29 +0700

    กลางวงงงง

--- lib/screen/displaylotto.dart
+++ lib/screen/displaylotto.dart
@@ -10,13 +10,23 @@ class DisplayScreen extends StatefulWidget {
 
 class _DisplayScreenState extends State<DisplayScreen> {
   //สร้าง List ไว้เก็บ Lottery
-  var snapshot;
-  var setDefaultvalue = true;
-  var dorpdownvalue;
+  var snapshot, _listDate;
+  List<DocumentSnapshot> documents;
+  Map<String, String> date = {};
+  String dateValue = "16 มิถุนายน 2564";
 
   @override
   void initState() {
     super.initState();
+    loadData();
+  }
+
+  Future loadData() async {
+    QuerySnapshot snapAll =
+        await FirebaseFirestore.instance.collection('lottery').get();
+    documents = snapAll.docs;
+    documents.forEach((data) => date[data.id] = data['date']);
+    print(date);
   }
 
   @override
@@ -36,162 +46,146 @@ class _DisplayScreenState extends State<DisplayScreen> {
           backgroundColor: Colors.black.withOpacity(0.1),
           elevation: 0,
         ),
-        body: Column(children: [
-          Expanded(
-              flex: 1,
-              child: Center(
-                child: new StreamBuilder<QuerySnapshot>(
-                    stream: FirebaseFirestore.instance
-                        .collection('lottery')
-                        .orderBy('date', descending: true)
-                        .snapshots(),
-                    builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                      if (!snapshot.hasData) return Container();
-                      if (setDefaultvalue) {
-                        dorpdownvalue = snapshot.data.docs[0].get('date');
-                        debugPrint('setDefault: $dorpdownvalue');
-                      }
-                      return DropdownButton(
-                        value: dorpdownvalue,
-                        icon: const Icon(Icons.arrow_downward),
-                        iconSize: 24,
-                        elevation: 16,
-                        style: const TextStyle(color: Colors.deepPurple),
-                        underline: Container(
-                          height: 2,
-                          color: Colors.deepPurpleAccent,
+        body: Center(
+          child: StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection('lottery')
+                  .doc(date.keys.firstWhere(
+                      (k) => date[k] == dateValue, //หา Keys โดยใช้ value
+                      orElse: () => null))
+                  .snapshots(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (!snapshot.hasData || !snapshot.data.exists) {
+                  return CircularProgressIndicator();
+                } else {
+                  return ListView(
+                    children: <Widget>[
+                      Container(
+                        alignment: AlignmentDirectional.topCenter,
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black26, width: 0.5),
+                            borderRadius: BorderRadius.circular(10)),
+                        padding: const EdgeInsets.only(top: 10.0),
+                        child: DropdownButtonHideUnderline(
+                          child: DropdownButton<String>(
+                          value: dateValue,
+                          icon: const Icon(Icons.arrow_drop_down),
+                          iconSize: 30,
+                          elevation: 2,
+                          
+                          style: TextStyle(color: Colors.blue,fontSize: 30),
+                          underline: Container(
+                            height: 2,
+                            color: Colors.blue,
+                          ),
+                          onChanged: (String newValue) {
+                            setState(() {
+                              dateValue = newValue;
+                            });
+                          },
+                          items: date.values
+                              .map<DropdownMenuItem<String>>((dynamic value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value,textAlign: TextAlign.right,),
+                            );
+                          }).toList(),
                         ),
-                        onChanged: (value) {
-                          debugPrint('selected onchange: $value');
-                          setState(() {
-                            debugPrint('make selected: $value');
-                            // Selected value will be stored
-                            dorpdownvalue = value;
-                            // Default dropdown value won't be displayed anymore
-                            setDefaultvalue = false;
-                          });
-                        },
-                        items: snapshot.data.docs.map((value) {
-                          return DropdownMenuItem(
-                            value: value.get('date'),
-                            child: Text('${value.get('date')}'),
-                          );
-                        }).toList(),
-                      );
-                    }),
-              )),
-          Expanded(
-              flex: 1,
-              child: Center(
-                  child: StreamBuilder(
-                      stream: FirebaseFirestore.instance
-                          .collection('lottery')
-                          .doc('25631216')
-                          .snapshots(),
-                      builder: (BuildContext context, AsyncSnapshot snapshot) {
-                        if (snapshot.data == null) {
-                          return CircularProgressIndicator();
-                        } else {
-                          return ListView(
-                            children: <Widget>[
-                              PrizeBox(
-                                  //รางวัลที่ 1
-                                  snapshot.data['prizes'][0]['name'],
-                                  snapshot.data['prizes'][0]['reward'],
-                                  snapshot.data['prizes'][0]['number'],
-                                  1,
-                                  30,
-                                  8),
-                              Row(
-                                //รางวัลเลขหน้า, เลขท้าย
-                                children: <Widget>[
-                                  Expanded(
-                                    child: PrizeBox(
-                                        snapshot.data['runningNumbers'][0]
-                                            ['name'],
-                                        snapshot.data['runningNumbers'][0]
-                                            ['reward'],
-                                        snapshot.data['runningNumbers'][0]
-                                            ['number'],
-                                        2,
-                                        25,
-                                        2),
-                                  ),
-                                  Expanded(
-                                    child: PrizeBox(
-                                        snapshot.data['runningNumbers'][1]
-                                            ['name'],
-                                        snapshot.data['runningNumbers'][1]
-                                            ['reward'],
-                                        snapshot.data['runningNumbers'][1]
-                                            ['number'],
-                                        2,
-                                        25,
-                                        2),
-                                  ),
-                                ],
-                              ),
-                              SizedBox(
-                                height: 10,
-                              ),
-                              PrizeBox(
-                                  //รางวัลเลขท้าย 2 ตัว
-                                  snapshot.data['runningNumbers'][2]['name'],
-                                  snapshot.data['runningNumbers'][2]['reward'],
-                                  snapshot.data['runningNumbers'][2]['number'],
-                                  1,
-                                  25,
-                                  9),
-                              PrizeBox(
-                                  //รางวัลใกล้เคียง รางวัลที่ 1
-                                  snapshot.data['prizes'][1]['name'],
-                                  snapshot.data['prizes'][1]['reward'],
-                                  snapshot.data['prizes'][1]['number'],
-                                  2,
-                                  20,
-                                  6),
-                              PrizeBox(
-                                  //รางวัลที่ 2
-                                  snapshot.data['prizes'][2]['name'],
-                                  snapshot.data['prizes'][2]['reward'],
-                                  snapshot.data['prizes'][2]['number'],
-                                  4,
-                                  20,
-                                  3),
-                              PrizeBox(
-                                  //รางวัลที่ 3
-                                  snapshot.data['prizes'][3]['name'],
-                                  snapshot.data['prizes'][3]['reward'],
-                                  snapshot.data['prizes'][3]['number'],
-                                  5,
-                                  18,
-                                  2),
-                              SizedBox(
-                                height: 10,
-                              ),
-                              PrizeBox(
-                                  //รางวัลที่ 4
-                                  snapshot.data['prizes'][4]['name'],
-                                  snapshot.data['prizes'][4]['reward'],
-                                  snapshot.data['prizes'][4]['number'],
-                                  5,
-                                  18,
-                                  2),
-                              SizedBox(
-                                height: 10,
-                              ),
-                              PrizeBox(
-                                  //รางวัลที่ 5
-                                  snapshot.data['prizes'][5]['name'],
-                                  snapshot.data['prizes'][5]['reward'],
-                                  snapshot.data['prizes'][5]['number'],
-                                  5,
-                                  18,
-                                  2),
-                            ],
-                          );
-                        }
-                      })))
-        ]));
+                        ),
+                        
+                      ),
+                      PrizeBox(
+                          //รางวัลที่ 1
+                          snapshot.data['prizes'][0]['name'],
+                          snapshot.data['prizes'][0]['reward'],
+                          snapshot.data['prizes'][0]['number'],
+                          1,
+                          30,
+                          8),
+                      Row(
+                        //รางวัลเลขหน้า, เลขท้าย
+                        children: <Widget>[
+                          Expanded(
+                            child: PrizeBox(
+                                snapshot.data['runningNumbers'][0]['name'],
+                                snapshot.data['runningNumbers'][0]['reward'],
+                                snapshot.data['runningNumbers'][0]['number'],
+                                2,
+                                25,
+                                2),
+                          ),
+                          Expanded(
+                            child: PrizeBox(
+                                snapshot.data['runningNumbers'][1]['name'],
+                                snapshot.data['runningNumbers'][1]['reward'],
+                                snapshot.data['runningNumbers'][1]['number'],
+                                2,
+                                25,
+                                2),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      PrizeBox(
+                          //รางวัลเลขท้าย 2 ตัว
+                          snapshot.data['runningNumbers'][2]['name'],
+                          snapshot.data['runningNumbers'][2]['reward'],
+                          snapshot.data['runningNumbers'][2]['number'],
+                          1,
+                          25,
+                          9),
+                      PrizeBox(
+                          //รางวัลใกล้เคียง รางวัลที่ 1
+                          snapshot.data['prizes'][1]['name'],
+                          snapshot.data['prizes'][1]['reward'],
+                          snapshot.data['prizes'][1]['number'],
+                          2,
+                          20,
+                          6),
+                      PrizeBox(
+                          //รางวัลที่ 2
+                          snapshot.data['prizes'][2]['name'],
+                          snapshot.data['prizes'][2]['reward'],
+                          snapshot.data['prizes'][2]['number'],
+                          4,
+                          20,
+                          3),
+                      PrizeBox(
+                          //รางวัลที่ 3
+                          snapshot.data['prizes'][3]['name'],
+                          snapshot.data['prizes'][3]['reward'],
+                          snapshot.data['prizes'][3]['number'],
+                          5,
+                          18,
+                          2),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      PrizeBox(
+                          //รางวัลที่ 4
+                          snapshot.data['prizes'][4]['name'],
+                          snapshot.data['prizes'][4]['reward'],
+                          snapshot.data['prizes'][4]['number'],
+                          5,
+                          18,
+                          2),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      PrizeBox(
+                          //รางวัลที่ 5
+                          snapshot.data['prizes'][5]['name'],
+                          snapshot.data['prizes'][5]['reward'],
+                          snapshot.data['prizes'][5]['number'],
+                          5,
+                          18,
+                          2),
+                    ],
+                  );
+                }
+              }),
+        ));
   }
 }
