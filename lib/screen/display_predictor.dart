4fb3b1dd706e11c12fb1bb1d30814c3d2babbf8b
yosaphon <68890091+yosaphon@users.noreply.github.com>
2021-07-22 15:28:42 +0700

    predition

--- lib/screen/display_predictor.dart
+++ lib/screen/display_predictor.dart
@@ -4,25 +4,43 @@ class DispalyPredictor extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          "ใบ้รางวัล",
-          style: TextStyle(color: Colors.black),
+        extendBodyBehindAppBar: false,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            "ใบ้รางวัล",
+            style: TextStyle(color: Colors.black),
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(16)),
+          ),
+          backgroundColor: Colors.black.withOpacity(0.1),
+          elevation: 0,
         ),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(bottom: Radius.circular(16)),
-        ),
-        backgroundColor: Colors.black.withOpacity(0.1),
-        elevation: 0,
-      ),
-      body: Stack(
-        children: [
-          
-        ],        
-      ),
-
-    );
+        body: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              SizedBox(height: 20,),
+              Container(
+                
+                height: 150.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                  color: Colors.blue[100],
+                ),
+              ),
+              SizedBox(height: 20,),
+              Container(
+                
+                height: 150.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                  color: Colors.blue[100],
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
