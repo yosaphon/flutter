34f13659f5c52fcebefac8ad3641d3e7b4efb715
Chaiwat Kanyasai <chaiwat.kulyasai7@gmail.com>
2021-07-23 11:57:23 +0700

    ฟังค์ชั่นตรวจ

--- lib/model/check_dialog.dart
+++ lib/model/check_dialog.dart
@@ -5,7 +5,9 @@ import 'package:flutter/material.dart';
 class CheckDialog {
   final String date;
   final List<String> userNum;
-  String name= "ไม่ถูกรางวัล", number="", reward="";
+  Map<int, Map<String, dynamic>> checked = {};
+  Map<String, dynamic> result = {};
+  int key = 0;
 
   CheckDialog(this.date, this.userNum);
 
@@ -13,26 +15,117 @@ class CheckDialog {
     DocumentSnapshot snapshot =
         await FirebaseFirestore.instance.collection('lottery').doc(date).get();
 
-    snapshot['prizes'].forEach((index) {
-      index['number'].forEach((a) {
-        if (a == userNum) {
-          name = index['name'];
-          number = a;
-          reward = index['reward'];
-        }
-      });
+    checkPrize(snapshot);
+    print(checked);
+  }
+
+  checkPrize(snapshot) async {
+    userNum.forEach((usernumber) {
+      int keyNow = key;
+      checkNormal(snapshot, usernumber);//ตรวจรางวัลปกติ
+      checkFirst3(snapshot, usernumber);//ตรวจรางวัล 3 ตัวหน้า
+      checkLast3(snapshot, usernumber);//ตรวจรางวัล 3 ตัวท้าย
+      checkLast2(snapshot, usernumber);//ตรวจรางวัล 2 ตัวท้าย
+      if (keyNow == key) { //ถ้าไม่มีข้อมูลที่ถูกเลย
+        result = {
+          'date': snapshot['drawdate'],
+          'usernumber': usernumber,
+          'name': '',
+          'number': '',
+          'reword': '',
+          'status': 'false'
+        };
+        checked[key] = result;
+        key++;
+      }
     });
-    showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-              title: ListTile(
-                title: Text(userNum.first),
-                subtitle: Text('$name มูลค่า $reward บาท'),
-              ),
-              actions: [
-                TextButton(
-                    onPressed: () => Navigator.pop(context), child: Text("OK"))
-              ],
-            ));
+  }
+
+  checkNormal(snapshot, usernumber) {
+    snapshot['result'].forEach((index) {
+      try { //รางวัลอื่นๆ
+        for (final item in index['number']) {
+          if (usernumber == item) {
+            result = {
+              'date': snapshot['drawdate'],
+              'usernumber': usernumber,
+              'name': index['name'],
+              'number': item,
+              'reword': index['reword'],
+              'status': 'true'
+            };
+            checked[key] = result;
+            key++;
+          }
+        }
+      } catch (e) { //รางวัลที่ 1
+        if (usernumber == index['number']) {
+          result = {
+            'date': snapshot['drawdate'],
+            'usernumber': usernumber,
+            'name': index['name'],
+            'number': index['number'],
+            'reword': index['reword'],
+            'status': 'true'
+          };
+          checked[key] = result;
+          key++;
+        }
+      }
+    });
+  }
+
+  checkFirst3(snapshot, usernumber) {
+    String f3Num = usernumber.substring(0, 3); //ตัดเอาเลข 3 ตัวหน้า
+    for (final item in snapshot['result'][1]['number']) {
+      if (f3Num == item) {
+        result = {
+          'date': snapshot['drawdate'],
+          'usernumber': usernumber,
+          'name': snapshot['result'][1]['name'],
+          'number': item,
+          'reword': snapshot['result'][1]['reword'],
+          'status': 'true'
+        };
+        checked[key] = result;
+        key++;
+        //print(result);
+      }
+    }
+  }
+
+  checkLast3(snapshot, usernumber) {
+    String text = usernumber.substring(3, 6); //ตัดเอาเลข 3 ตัวหลัง
+    for (final item in snapshot['result'][2]['number']) {
+      if (text == item) {
+        result = {
+          'date': snapshot['drawdate'],
+          'usernumber': usernumber,
+          'name': snapshot['result'][2]['name'],
+          'number': item,
+          'reword': snapshot['result'][2]['reword'],
+          'status': 'true'
+        };
+        checked[key] = result;
+        key++;
+      }
+    }
+  }
+
+  checkLast2(snapshot, usernumber) {
+    String text = usernumber.substring(4, 6); //ตัดเอาเลข 3 ตัวหลัง
+    String number = snapshot['result'][3]['number'];
+    if (text == number) {
+      result = {
+        'date': snapshot['drawdate'],
+        'usernumber': usernumber,
+        'name': snapshot['result'][3]['name'],
+        'number': number,
+        'reword': snapshot['result'][3]['reword'],
+        'status': 'true'
+      };
+      checked[key] = result;
+      key++;
+    }
   }
 }
