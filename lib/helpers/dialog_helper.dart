80dc8ac566bb2e9274f0194bf995e1c4ec5a611d
Chaiwat Kanyasai <chaiwat.kulyasai7@gmail.com>
2021-07-24 10:46:35 +0700

    ตัวเรียก Dialog แสดง

---
+++ lib/helpers/dialog_helper.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:lotto/widgets/checkedDialog.dart';
+
+
+class DialogHelper {
+  
+  static exit(context,data) =>
+      showDialog(context: context, builder: (context) => CheckedDialog(data));
+}
