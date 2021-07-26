94cff4187165beb36c70d177e47387ff6dafc3ad
Chaiwat Kanyasai <chaiwat.kulyasai7@gmail.com>
2021-07-26 09:05:54 +0700

    เปลี่ยนที่

--- lib/screen/displaylotto.dart
+++ lib/screen/displaylotto.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:lotto/model/prizeBox.dart';
+import 'package:lotto/widgets/prizeBox.dart';
 import 'package:lotto/screen/showCheckImage.dart';
 
 class DisplayScreen extends StatefulWidget {
