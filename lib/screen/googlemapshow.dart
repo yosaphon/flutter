f13dfc7e59ba62934af3795171aae00c302e6a0b
yosaphon <68890091+yosaphon@users.noreply.github.com>
2021-07-31 14:31:22 +0700

    อัปใหม่ๆ

--- lib/screen/googlemapshow.dart
+++ lib/screen/googlemapshow.dart
@@ -1,33 +1,105 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class showGooglemap extends StatelessWidget {
+class showuserGooglemap extends StatelessWidget {
+  GoogleMapController mapController;
+  Position userlocation;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(
-              title: Text('Maps Sample App'),
-              backgroundColor: Colors.green[700],
-            ),
-            body: Container(
-              height: 100,
-              width: double.infinity,
-              margin: EdgeInsets.only(left: 30, right: 30),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                border: Border.all(
-                  style: BorderStyle.solid,
-                ),
-              ),
-                child: GoogleMap(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        centerTitle: true,
+        title: Text(
+          "google map",
+          style: TextStyle(color: Colors.black),
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(bottom: Radius.circular(16)),
+        ),
+        // backgroundColor: Colors.transparent,
+        backgroundColor: Colors.black.withOpacity(0.1),
+        elevation: 0,
+      ),
+      body: FutureBuilder(
+        future: _getuserlocation(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasData) {
+            return Stack(
+              children: [
+                GoogleMap(
                   mapType: MapType.normal,
                   initialCameraPosition: CameraPosition(
-                    target: LatLng(24.142, -110.321),
+                    target:
+                        LatLng(userlocation.latitude, userlocation.longitude),
                     zoom: 15,
                   ),
+                  onMapCreated: _onMapCreated,
+                  myLocationEnabled: true,
+                  compassEnabled: true,
                 ),
-              ),
-            ));
+              ],
+            );
+          } else {
+            return _buildGoogleMap(context);
+          }
+        },
+      ),
+    ));
+  }
+
+  Widget _buildGoogleMap(BuildContext context) {
+    return Stack(
+      children: [
+        GoogleMap(
+          mapType: MapType.normal,
+          initialCameraPosition: CameraPosition(
+            target: LatLng(13.7535, 100.5237),
+            zoom: 15,
+          ),
+          onMapCreated: _onMapCreated,
+          myLocationEnabled: true,
+          compassEnabled: true,
+        ),
+        // Positioned(
+        //   bottom: 50,
+        //   right: 10,
+        //   child: IconButton(
+        //     icon: Icon(Icons.pin_drop),
+        //     color: Colors.blue,
+        //     onPressed: () {
+        //       _addmarker();
+        //     },
+        //   ),
+        // )
+      ],
+    );
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    // setState(() {
+    mapController = controller;
+    // });
+  }
+
+  Future<Position> _getuserlocation() async {
+    try {
+      userlocation = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.best);
+    } catch (e) {
+      userlocation = null;
+    }
+    return userlocation;
+  }
+
+  _addmarker() {
+    var marker = Marker(
+        markerId: const MarkerId('location'),
+        infoWindow: const InfoWindow(title: 'location'));
   }
 }
