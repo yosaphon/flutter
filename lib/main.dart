f927eedbecea1ac7e520821618b77633c2c4ddf8
yosaphon <68890091+yosaphon@users.noreply.github.com>
2021-09-22 17:36:00 +0700

    navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -53,36 +54,71 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int selectIndex = 0;
+  final scren = [
+    DisplayScreen(),
+    Formqrcodescan(),
+    DisplayLiveYoutube(),
+    DispalyPredictor(),
+    CheckLogInUser()
+  ];
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-        length: 5,
-        child: Scaffold(
-          body: TabBarView(
-            physics: NeverScrollableScrollPhysics(),
-            children: [
-              DisplayScreen(),
-              Formqrcodescan(),
-              DisplayLiveYoutube(),
-              DispalyPredictor(),
-              CheckLogInUser()
-            ],
-          ),
-          backgroundColor: Colors.blue,
-          bottomNavigationBar: TabBar(
-            labelStyle: TextStyle(fontSize: 10.0),
-            labelColor: Color(0xffffffff), // สีของข้อความปุ่มที่เลือก
+    return Scaffold(
+      bottomNavigationBar: CurvedNavigationBar(
+        index: selectIndex,
+        color: Colors.blue,
+        buttonBackgroundColor: Colors.blue,
+        backgroundColor: Colors.white60,
+        animationCurve: Curves.easeInOut,
+        items: <Widget>[
+          // Tab(text: "หน้าแรก", icon:
+          Icon(Icons.home,size: 30,),
+          // Tab(text: "ตรวจสลาก", icon:
+          Icon(Icons.qr_code_2,size: 30,),
+          // Tab(text: "ถ่ายทอดสด", icon:
+          FaIcon(FontAwesomeIcons.youtube,size: 30,),
+          // Tab(text: "ใบ้รางวัล", icon:
+          Icon(Icons.online_prediction,size: 30,),
+          // Tab(text: "ผู้ใช้", icon:
+          Icon(Icons.account_circle_outlined,size: 30,)
+        ],
+        onTap: (index) {
+          setState(() {
+            selectIndex = index;
+          });
+        },
+      ),
+      body: scren[selectIndex],
+    );
+    // return DefaultTabController(
+    //     length: 5,
+    //     child: Scaffold(
+    //       body: TabBarView(
+    //         physics: NeverScrollableScrollPhysics(),
+    //         children: [
+    //           DisplayScreen(),
+    //           Formqrcodescan(),
+    //           DisplayLiveYoutube(),
+    //           DispalyPredictor(),
+    //           CheckLogInUser()
+    //         ],
+    //       ),
+    //       backgroundColor: Colors.blue,
+    //       bottomNavigationBar: TabBar(
+    //         labelStyle: TextStyle(fontSize: 10.0),
+    //         labelColor: Color(0xffffffff), // สีของข้อความปุ่มที่เลือก
 
-            unselectedLabelColor:
-                Color(0x55ffffff), // สีของข้อความปุ่มที่ไม่ได้เลือก
-            tabs: [
-              Tab(text: "หน้าแรก", icon: Icon(Icons.home)),
-              Tab(text: "ตรวจสลาก", icon: Icon(Icons.qr_code_2)),
-              Tab(text: "ถ่ายทอดสด", icon: FaIcon(FontAwesomeIcons.youtube)),
-              Tab(text: "ใบ้รางวัล", icon: Icon(Icons.online_prediction)),
-              Tab(text: "ผู้ใช้", icon: Icon(Icons.account_circle_outlined))
-            ],
-          ),
-        ));
+    //         unselectedLabelColor:
+    //             Color(0x55ffffff), // สีของข้อความปุ่มที่ไม่ได้เลือก
+    //         tabs: [
+    //           Tab(text: "หน้าแรก", icon: Icon(Icons.home)),
+    //           Tab(text: "ตรวจสลาก", icon: Icon(Icons.qr_code_2)),
+    //           Tab(text: "ถ่ายทอดสด", icon: FaIcon(FontAwesomeIcons.youtube)),
+    //           Tab(text: "ใบ้รางวัล", icon: Icon(Icons.online_prediction)),
+    //           Tab(text: "ผู้ใช้", icon: Icon(Icons.account_circle_outlined))
+    //         ],
+    //       ),
+    //     ));
   }
 }
