82c3b0a45288ee4d03c7dacf405eecdc3236b194
yosaphon <68890091+yosaphon@users.noreply.github.com>
2021-09-27 18:09:33 +0700

    แก้อักษรติด

--- lib/screen/display_predictor.dart
+++ lib/screen/display_predictor.dart
@@ -65,17 +65,6 @@ class _DispalyPredictorState extends State<DispalyPredictor> {
   bool isBack = true;
   double angle1 = 0, angle2 = 0;
 
-  void _flip1() {
-    setState(() {
-      angle1 = (angle1 + pi) % (2 * pi);
-    });
-  }
-
-  void _flip2() {
-    setState(() {
-      angle2 = (angle2 + pi) % (2 * pi);
-    });
-  }
 
   String numToWord(String n) {
     List<String> month = [
@@ -178,6 +167,7 @@ class _DispalyPredictorState extends State<DispalyPredictor> {
                                         BorderRadius.all(Radius.circular(20))),
                                     child: Column(
                                       children: [
+                                        SizedBox(height: 15,),
                                         Text(document.title,style: TextStyle(
                                                       fontSize: 18,
                                                       color: Colors.black87)),
@@ -210,197 +200,7 @@ class _DispalyPredictorState extends State<DispalyPredictor> {
                         ),
                       ),
                     ),
-
-                    // Row(
-                    //   mainAxisAlignment: MainAxisAlignment.center,
-                    //   children: <Widget>[
-                    //     GestureDetector(
-                    //       onTap: _flip1,
-                    //       child: TweenAnimationBuilder(
-                    //           tween: Tween<double>(begin: 0, end: angle1),
-                    //           duration: Duration(milliseconds: 400),
-                    //           builder: (BuildContext context, double val, __) {
-                    //             //here we will change the isBack val so we can change the content of the card
-                    //             if (val >= (pi / 2)) {
-                    //               isBack = false;
-                    //             } else {
-                    //               isBack = true;
-                    //             }
-                    //             return (Transform(
-                    //               //let's make the card flip by it's center
-                    //               alignment: Alignment.center,
-                    //               transform: Matrix4.identity()
-                    //                 ..setEntry(3, 2, 0.001)
-                    //                 ..rotateY(val),
-                    //               child: Container(
-                    //                   height:
-                    //                       MediaQuery.of(context).size.height /
-                    //                           2.15,
-                    //                   width: MediaQuery.of(context).size.width /
-                    //                       2.15,
-                    //                   child: isBack
-                    //                       ? Container(
-                    //                           decoration: BoxDecoration(
-                    //                             borderRadius:
-                    //                                 BorderRadius.circular(10.0),
-                    //                             image: DecorationImage(
-                    //                               image: AssetImage(
-                    //                                   "asset/back.png"),
-                    //                             ),
-                    //                           ),
-                    //                         ) //if it's back we will display here
-                    //                       : Transform(
-                    //                           alignment: Alignment.center,
-                    //                           transform: Matrix4.identity()
-                    //                             ..rotateY(
-                    //                                 pi), // it will flip horizontally the container
-                    //                           child: Container(
-                    //                             decoration: BoxDecoration(
-                    //                               borderRadius:
-                    //                                   BorderRadius.circular(
-                    //                                       10.0),
-                    //                               image: DecorationImage(
-                    //                                 image: AssetImage(
-                    //                                     "asset/face.png"),
-                    //                               ),
-                    //                             ),
-                    //                             child: Center(
-                    //                               child: lottoerypredition(
-                    //                                   //ส่งค่า รางวัลไปคำนวณ ที่lottoerypredition
-                    //                                   prizeNotifier
-                    //                                       .selectedPrize
-                    //                                       .data['first']
-                    //                                       .number[0]
-                    //                                       .value,
-                    //                                   prizeNotifier
-                    //                                       .selectedPrize
-                    //                                       .data['last2']
-                    //                                       .number[0]
-                    //                                       .value,
-                    //                                   prizeNotifier
-                    //                                       .selectedPrize
-                    //                                       .data['last3f']
-                    //                                       .number[0]
-                    //                                       .value,
-                    //                                   prizeNotifier
-                    //                                       .selectedPrize
-                    //                                       .data['last3f']
-                    //                                       .number[1]
-                    //                                       .value,
-                    //                                   prizeNotifier
-                    //                                       .selectedPrize
-                    //                                       .data['last3b']
-                    //                                       .number[0]
-                    //                                       .value,
-                    //                                   prizeNotifier
-                    //                                       .selectedPrize
-                    //                                       .data['last3b']
-                    //                                       .number[1]
-                    //                                       .value,
-                    //                                   "1"),
-                    //                             ),
-                    //                           ),
-                    //                         ) //else we will display it here,
-                    //                   ),
-                    //             ));
-                    //           }),
-                    //     ),
-                    //     SizedBox(
-                    //       width: 5,
-                    //     ),
-                    //     GestureDetector(
-                    //       onTap: _flip2,
-                    //       child: TweenAnimationBuilder(
-                    //           tween: Tween<double>(begin: 0, end: angle2),
-                    //           duration: Duration(milliseconds: 400),
-                    //           builder: (BuildContext context, double val, __) {
-                    //             //here we will change the isBack val so we can change the content of the card
-                    //             if (val >= (pi / 2)) {
-                    //               isBack = false;
-                    //             } else {
-                    //               isBack = true;
-                    //             }
-                    //             return (Transform(
-                    //               //let's make the card flip by it's center
-                    //               alignment: Alignment.center,
-                    //               transform: Matrix4.identity()
-                    //                 ..setEntry(3, 2, 0.001)
-                    //                 ..rotateY(val),
-                    //               child: Container(
-                    //                   height:
-                    //                       MediaQuery.of(context).size.height /
-                    //                           2.15,
-                    //                   width: MediaQuery.of(context).size.width /
-                    //                       2.15,
-                    //                   child: isBack
-                    //                       ? Container(
-                    //                           decoration: BoxDecoration(
-                    //                             borderRadius:
-                    //                                 BorderRadius.circular(10.0),
-                    //                             image: DecorationImage(
-                    //                               image: AssetImage(
-                    //                                   "asset/back.png"),
-                    //                             ),
-                    //                           ),
-                    //                         ) //if it's back we will display here
-                    //                       : Transform(
-                    //                           alignment: Alignment.center,
-                    //                           transform: Matrix4.identity()
-                    //                             ..rotateY(
-                    //                                 pi), // it will flip horizontally the container
-                    //                           child: Container(
-                    //                             decoration: BoxDecoration(
-                    //                               borderRadius:
-                    //                                   BorderRadius.circular(
-                    //                                       10.0),
-                    //                               image: DecorationImage(
-                    //                                 image: AssetImage(
-                    //                                     "asset/face.png"),
-                    //                               ),
-                    //                             ),
-                    //                             child: Center(
-                    //                               child: lottoerypredition(
-                    //                                   //ส่งค่า รางวัลไปคำนวณ ที่lottoerypredition
-                    //                                   prizeNotifier
-                    //                                       .selectedPrize
-                    //                                       .data['first']
-                    //                                       .number[0]
-                    //                                       .value,
-                    //                                   prizeNotifier
-                    //                                       .selectedPrize
-                    //                                       .data['last2']
-                    //                                       .number[0]
-                    //                                       .value,
-                    //                                   prizeNotifier
-                    //                                       .selectedPrize
-                    //                                       .data['last3f']
-                    //                                       .number[0]
-                    //                                       .value,
-                    //                                   prizeNotifier
-                    //                                       .selectedPrize
-                    //                                       .data['last3f']
-                    //                                       .number[1]
-                    //                                       .value,
-                    //                                   prizeNotifier
-                    //                                       .selectedPrize
-                    //                                       .data['last3b']
-                    //                                       .number[0]
-                    //                                       .value,
-                    //                                   prizeNotifier
-                    //                                       .selectedPrize
-                    //                                       .data['last3b']
-                    //                                       .number[1]
-                    //                                       .value,
-                    //                                   "2"),
-                    //                             ),
-                    //                           ),
-                    //                         ) //else we will display it here,
-                    //                   ),
-                    //             ));
-                    //           }),
-                    //     )
-                    //   ],
-                    // ),
+                    
                   ],
                 );
               }
