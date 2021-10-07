e891d5f49c756a545f4fc90d947bc8958fe279d3
yosaphon <68890091+yosaphon@users.noreply.github.com>
2021-10-07 16:20:00 +0700

    intro

---
+++ lib/screen/onboard/onboard.dart
@@ -0,0 +1,201 @@
+
+import 'package:flutter/material.dart';
+import 'package:lotto/main.dart';
+import 'package:lotto/model/onboardModel.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class OnBoard extends StatefulWidget {
+  @override
+  _OnBoardState createState() => _OnBoardState();
+}
+
+class _OnBoardState extends State<OnBoard> {
+Color kblue = Color(0xFF4756DF);
+Color kwhite = Color(0xFFFFFFFF);
+Color kblack = Color(0xFF000000);
+Color kbrown300 = Color(0xFF8D6E63);
+Color kbrown = Color(0xFF795548);
+  int currentIndex = 0;
+  PageController _pageController;
+  List<OnboardModel> screens = <OnboardModel>[
+    OnboardModel(
+      img: 'asset/guraLottery.png',
+      text: "ผลการออกรางวัล",
+      desc:
+          "สามารถดูผลการออกรางวัลในงวดปัจจุบันและสามารถดูผลกการออกรางวัลย้อนหลังได้",
+      bg: Colors.white,
+      button: Color(0xFF4756DF),
+    ),
+    OnboardModel(
+      img: 'asset/guraLottery.png',
+      text: "ลิงค์ดูถ่ายทอดสด",
+      desc:
+          "สามารถเปิดดูการถ่ายทอดสดในปัจจุบันและอดีตได้",
+      bg: Color(0xFF4756DF),
+      button: Colors.white,
+    ),
+    OnboardModel(
+      img: 'asset/guraLottery.png',
+      text: "ตรวจผลรางวัล",
+      desc:
+          "สามารถตรวจรางวัลด้วยการ สแกน Data Matrix 2D Code หรือ กรอกเลขตรวจได้",
+      bg: Color(0xFF4756DF),
+      button: Colors.white,
+    ),
+    OnboardModel(
+      img: 'asset/guraLottery.png',
+      text: "บันทึกสลาก",
+      desc:
+          "สามารถบันทึกสลากก่อนออกรางวัลได้และเมือรางวัลออกระบบจะส่งแจ้งเตือนไปยังมือถือ",
+      bg: Colors.white,
+      button: Color(0xFF4756DF),
+    ),
+  ];
+
+  @override
+  void initState() {
+    _pageController = PageController(initialPage: 0);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  _storeOnboardInfo() async {
+    print("Shared pref called");
+    int isViewed = 0;
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('onBoard', isViewed);
+    print(prefs.getInt('onBoard'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: currentIndex % 2 == 0 ? kwhite : kblue,
+      appBar: AppBar(
+        backgroundColor: currentIndex % 2 == 0 ? kwhite : kblue,
+        elevation: 0.0,
+        actions: [
+          TextButton(
+            onPressed: () {
+              _storeOnboardInfo();
+              Navigator.pushReplacement(
+                  context, MaterialPageRoute(builder: (context) => MyHomePage()));
+            },
+            child: Text(
+              "Skip",
+              style: TextStyle(
+                color: currentIndex % 2 == 0 ? kblack : kwhite,
+              ),
+            ),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: PageView.builder(
+            itemCount: screens.length,
+            controller: _pageController,
+            physics: NeverScrollableScrollPhysics(),
+            onPageChanged: (int index) {
+              setState(() {
+                currentIndex = index;
+              });
+            },
+            itemBuilder: (_, index) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.asset(screens[index].img,width: 100,height: 100,),
+                  Container(
+                    height: 10.0,
+                    child: ListView.builder(
+                      itemCount: screens.length,
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        return Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                margin: EdgeInsets.symmetric(horizontal: 3.0),
+                                width: currentIndex == index ? 25 : 8,
+                                height: 8,
+                                decoration: BoxDecoration(
+                                  color: currentIndex == index
+                                      ? kbrown
+                                      : kbrown300,
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                              ),
+                            ]);
+                      },
+                    ),
+                  ),
+                  Text(
+                    screens[index].text,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 27.0,
+                      fontWeight: FontWeight.bold,
+                      color: index % 2 == 0 ? kblack : kwhite,
+                    ),
+                  ),
+                  Text(
+                    screens[index].desc,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      color: index % 2 == 0 ? kblack : kwhite,
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () async {
+                      print(index);
+                      if (index == screens.length - 1) {
+                        await _storeOnboardInfo();
+                        Navigator.pushReplacement(context,
+                            MaterialPageRoute(builder: (context) => MyHomePage()));
+                      }
+
+                      _pageController.nextPage(
+                        duration: Duration(milliseconds: 300),
+                        curve: Curves.bounceIn,
+                      );
+                    },
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 30.0, vertical: 10),
+                      decoration: BoxDecoration(
+                          color: index % 2 == 0 ? kblue : kwhite,
+                          borderRadius: BorderRadius.circular(15.0)),
+                      child: Row(mainAxisSize: MainAxisSize.min, children: [
+                        Text(
+                         index == screens.length-1? "เริ่มต้นใช้งาน":"Next",
+                          style: TextStyle(
+                              fontSize: 16.0,
+                              color: index % 2 == 0 ? kwhite : kblue),
+                        ),
+                        SizedBox(
+                          width: 15.0,
+                        ),
+                        Icon(
+                          Icons.arrow_forward_sharp,
+                          color: index % 2 == 0 ? kwhite : kblue,
+                        )
+                      ]),
+                    ),
+                  )
+                ],
+              );
+            }),
+      ),
+    );
+  }
+}
